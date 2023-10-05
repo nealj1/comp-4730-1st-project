075460e5ad8d5f8354306fcb2961736e8f1a3f74
cyoger <akshat1sharma2@gmail.com>
2023-10-05 02:32:30 -0400

    added hyperparameter search and 2 new classifiers

--- mushroom.py
+++ mushroom.py
@@ -1,20 +1,18 @@
 import pandas as pd
 import os 
 import matplotlib.pyplot as plt 
-import seaborn as sns
+import numpy as np
 
-from sklearn.gaussian_process.kernels import RBF
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.tree import plot_tree
 
 def read_data(path):
@@ -67,6 +65,21 @@ def train_and_evaluate_model(model, x_train, y_train, x_test, y_test, feature_na
     if isinstance(model, DecisionTreeClassifier):
         plot_decision_tree(model, feature_names, class_names)
 
+def hyperparameter_search(model, params, x_train, y_train, x_test, y_test, feature_names, class_names):
+    print(f"\nStarting {model.__class__.__name__}....")
+    
+    grid_search = GridSearchCV(model, params, cv=5, scoring='accuracy')
+    grid_search.fit(x_train, y_train)
+    
+    print(f"Best parameters for {model.__class__.__name__}: {grid_search.best_params_}")
+    
+    best_model = grid_search.best_estimator_
+    y_pred = best_model.predict(x_test)
+    
+    print_metrics(y_test, y_pred)
+
+
+
 def print_metrics(y_test, y_pred):
     
     accuracy = accuracy_score(y_test, y_pred)
@@ -107,10 +120,29 @@ if __name__ == "__main__":
         DecisionTreeClassifier(criterion="gini", max_depth=3, random_state=0),
         RandomForestClassifier(criterion="entropy", max_depth=4, random_state=0),
         GaussianNB(),
-        SVC(kernel="linear", C=0.025, random_state=42)
+        SVC(kernel="linear", C=0.025, random_state=42),
+        AdaBoostClassifier(random_state=42, n_estimators=10),
     ]
     
     
     for model in models: 
         train_and_evaluate_model(model, x_train, y_train, x_test, y_test, feature_names, class_names) #loop to train and evaluate 
+        cv_scores = cross_val_score(model, x_train, y_train, cv=5, scoring='accuracy') # 5-fold CV
+        print(f"{model.__class__.__name__} CV Accuracy: {np.mean(cv_scores):.2f} +/- {np.std(cv_scores):.2f}")
+        
+    print("\nPerforming hyperparameter search now...")
+    
+    models_with_params = [
+        (LogisticRegression(), {'C': [0.01, 0.1, 1, 10], 'penalty': ['l1', 'l2'], 'solver': ['liblinear', 'saga'], 'max_iter': [100, 200, 300]}),
+        (DecisionTreeClassifier(random_state=0), {'criterion': ['entropy','gini'], 'max_depth': [3, 5, 7, 9]}),
+        (RandomForestClassifier(random_state=0), {'n_estimators': [10, 50, 100], 'max_depth': [3, 5, 7]}),
+        (GaussianNB(), {'var_smoothing': [1e-9, 1e-8, 1e-10]}),
+        (SVC(random_state=42), {'C': [0.025, 0.25, 2.5], 'kernel': ['linear', 'rbf', 'poly'], 'degree': [2, 3, 4]}),
+        (AdaBoostClassifier(random_state=42), {'n_estimators': [10, 50, 100], 'learning_rate': [0.01, 0.1, 1]})
+        
+    ]
+
+    for model, params in models_with_params:
+        hyperparameter_search(model, params, x_train, y_train, x_test, y_test, feature_names, class_names)
+        
 
