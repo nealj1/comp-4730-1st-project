2241e04b1a6017996817eae0b68dd63c5cd24b90
nealj <nealj@uwindsor.ca>
2023-10-08 19:30:24 -0400

    Work on graphs

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
+import printing as dataPrinting
+import matplotlib.pyplot as plt
+import seaborn as sns
 #SETTINGS ---------------------------------------------------------------
 pd.set_option('display.max_colwidth', None)
+# Set the display option to show all rows
+#pd.set_option('display.max_rows', None)
 
 # Functions -------------------------------------------------------------
-
-
-# Load the dataset
-mushroom_data = pd.read_csv('Mushroom/agaricus-lepiota.csv')
-
 def summary(df):
     summary_df = pd.DataFrame(df.dtypes, columns=['dtypes'])
     summary_df['duplicated'] = df.duplicated().sum()
@@ -47,9 +51,27 @@ def columns_with_question_marks(df):
             columns_with_question_mark.append(column)
     return columns_with_question_mark
 
+def balance_dataset(mushroom_data, rows_to_remove):
+    # Find rows where 'class' is "EDIBLE" and 'stalk-root' is "?"
+    edible_with_question_mark_stalk_root = mushroom_data[(mushroom_data['class'] == 'EDIBLE') & (mushroom_data['stalk-root'] == '?')]
+    # Get the indices of the rows to remove
+    indices_to_remove = edible_with_question_mark_stalk_root.index[:rows_to_remove]
+    # Remove the specified rows from the DataFrame
+    mushroom_data = mushroom_data.drop(indices_to_remove)
+    # Verify that the rows have been removed
+    print("Number of rows removed:", len(indices_to_remove))
+    return mushroom_data
+
+# Main --------------------------------------------------------------------
+# Load the dataset
+mushroom_data = pd.read_csv('Mushroom/agaricus-lepiota.csv')
+
 # Print or display the summary DataFrame
 df_summary = summary(mushroom_data)
 print(df_summary)
+
+# Dropped duplicate rows
+mushroom_data = mushroom_data.drop_duplicates()
 
 # Check for ? in the data
 question_mark_columns = columns_with_question_marks(mushroom_data)
@@ -57,41 +79,78 @@ for column_name in question_mark_columns:
     count_question_marks = mushroom_data[column_name].str.count("\?").sum()
     print(f"Number of '?' in '{column_name}': {count_question_marks}")
 
+
+print(f"HERE : {mushroom_data.isnull().sum()}")
+
+
+# Find rows where 'class' is "EDIBLE" and 'stalk-root' is "?"
+edible_with_question_mark_stalk_root = mushroom_data[(mushroom_data['class'] == 'EDIBLE') & (mushroom_data['stalk-root'] == '?')]
+edible = mushroom_data[(mushroom_data['class'] == 'EDIBLE')]
+poisonous = mushroom_data[(mushroom_data['class'] == 'POISONOUS')]
+dataPrinting.print_class_compare(mushroom_data, "Distribution of Mushroom Classes")
+
+# Balancing
+mushroom_data = balance_dataset(mushroom_data, edible.shape[0] - poisonous.shape[0])
+print(f'mushroom count: {edible.shape[0] - poisonous.shape[0]}')
+dataPrinting.print_class_compare(mushroom_data,"Distribution of Mushroom Classes Balanced" )
+
 #Drop Columns
 mushroom_data = mushroom_data.drop(columns=['stalk-root']) # column has lots of ?
 mushroom_data = mushroom_data.drop(columns=['veil-type']) # is a constant column so does not add anything
 
-#Encodes
-mushroom_data = pd.get_dummies(mushroom_data)
-#or this one ????
-'''
-# Encode categorical features
+
+#Encodes categorical features
 label_encoder = LabelEncoder()
 for column in mushroom_data.columns:
     mushroom_data[column] = label_encoder.fit_transform(mushroom_data[column])
-'''
+
+dataPrinting.print_heatmap(mushroom_data)
 
 
-
-# Do ai training stuff
-'''
 # Split the data into train and test sets
 X = mushroom_data.drop('class', axis=1)
 y = mushroom_data['class']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create and train a Random Forest classifier
-clf = RandomForestClassifier(n_estimators=100, random_state=42)
-clf.fit(X_train, y_train)
+# Define a list of models
+models = [
+    RandomForestClassifier(random_state=42),
+    LogisticRegression(random_state=42),
+    SVC(random_state=42),
+    DecisionTreeClassifier(random_state=42),
+    GaussianNB()
+]
 
-# Predict on the test set
-y_pred = clf.predict(X_test)
+# Split the data into train and test sets
+X = mushroom_data.drop('class', axis=1)
+y = mushroom_data['class']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
-print(classification_report(y_test, y_pred))
-print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
-'''
+# Lists to store model names and their corresponding accuracies
+model_names = []
+accuracies = []
 
-print(mushroom_data.count)
+# Iterate through the list of models and train/evaluate each one
+for model in models:
+    # Train the model on the training data
+    model.fit(X_train, y_train)
+
+    # Make predictions on the test data
+    y_pred = model.predict(X_test)
+
+    # Evaluate the model's performance
+    accuracy = accuracy_score(y_test, y_pred)
+    confusion = confusion_matrix(y_test, y_pred)
+    report = classification_report(y_test, y_pred)
+    # Store model name and accuracy
+    model_names.append(model.__class__.__name__)
+    accuracies.append(accuracy)
+
+    # Print the results for each model
+    print(f"Model: {model.__class__.__name__}")
+    print("Accuracy:", accuracy)
+    print("Confusion Matrix:\n", confusion)
+    print("Classification Report:\n", report)
+    print("\n")
+
+dataPrinting.print_compare_model_accuracies(model_names, accuracies)
--- printing.py
+++ printing.py
@@ -3,3 +3,43 @@
 # edible vs poisonous
 # we should do the cv like in the regression where we graph each of the fold values
 # print the decision tree graph to see what the nodes it split on are
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def print_class_compare(mushroom_data, title):
+    # Count the frequency of each class
+    class_counts = mushroom_data['class'].value_counts()
+
+    # Plot the bar graph
+    plt.figure(figsize=(6, 4))  # Adjust the figure size as needed
+    ax = class_counts.plot(kind='bar', color=['green', 'red'])  # Customize colors if desired
+
+    # Define custom labels for the x-axis ticks
+    custom_labels = ['EDIBLE', 'POISONOUS']  # Replace with your desired labels
+
+    # Set the custom labels for the x-axis ticks
+    plt.xticks(range(len(custom_labels)), custom_labels)
+
+    plt.title(title)
+    plt.xlabel('Class')
+    plt.ylabel('Count')
+    plt.show()
+
+def print_compare_model_accuracies(model_names, accuracies):
+    plt.figure(figsize=(10, 6))
+    plt.bar(model_names, accuracies, color='skyblue')
+    plt.xlabel('Model')
+    plt.ylabel('Accuracy')
+    plt.title('Model Comparison - Accuracy')
+    plt.xticks(rotation=45)  # Rotate x-axis labels for readability
+    plt.tight_layout()
+
+    # Show the bar chart
+    plt.show()
+
+def print_heatmap(mushroom_data):
+    # check to get a rough idea of correlations
+    correlations = mushroom_data.corr()
+    plt.subplots(figsize=(20, 15))
+    data_corr = sns.heatmap(correlations, annot=True, linewidths=0.5, cmap="RdBu_r")
+    plt.show()
