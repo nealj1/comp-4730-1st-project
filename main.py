94bd94331cec9d343393baa79cb7e4ab851a614a
nealj <nealj@uwindsor.ca>
2023-10-09 19:49:01 -0400

    working on updating code, making better visual of data for paper

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
@@ -62,6 +62,8 @@ def balance_dataset(mushroom_data, rows_to_remove):
     print("Number of rows removed:", len(indices_to_remove))
     return mushroom_data
 
+
+
 # Main --------------------------------------------------------------------
 # Load the dataset
 mushroom_data = pd.read_csv('Mushroom/agaricus-lepiota.csv')
@@ -86,12 +88,12 @@ for column_name in question_mark_columns:
 edible_with_question_mark_stalk_root = mushroom_data[(mushroom_data['class'] == 'EDIBLE') & (mushroom_data['stalk-root'] == '?')]
 edible = mushroom_data[(mushroom_data['class'] == 'EDIBLE')]
 poisonous = mushroom_data[(mushroom_data['class'] == 'POISONOUS')]
-dataPrinting.print_class_compare(mushroom_data, "Distribution of Mushroom Classes")
+#dataPrinting.print_class_compare(mushroom_data, "Distribution of Mushroom Classes")
 
 # Balancing
 mushroom_data = balance_dataset(mushroom_data, edible.shape[0] - poisonous.shape[0])
 print(f'mushroom count: {edible.shape[0] - poisonous.shape[0]}')
-dataPrinting.print_class_compare(mushroom_data,"Distribution of Mushroom Classes Balanced" )
+#dataPrinting.print_class_compare(mushroom_data,"Distribution of Mushroom Classes Balanced" )
 
 #Drop Columns
 mushroom_data = mushroom_data.drop(columns=['stalk-root']) # column has lots of ?
@@ -103,7 +105,7 @@ label_encoder = LabelEncoder()
 for column in mushroom_data.columns:
     mushroom_data[column] = label_encoder.fit_transform(mushroom_data[column])
 
-dataPrinting.print_heatmap(mushroom_data)
+#dataPrinting.print_heatmap(mushroom_data)
 
 
 # Split the data into train and test sets
@@ -114,28 +116,33 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Define a list of models
 models = [
     RandomForestClassifier(random_state=42),
-    LogisticRegression(random_state=42),
+    LogisticRegression(random_state=42, max_iter=1000),
     SVC(random_state=42),
     DecisionTreeClassifier(random_state=42),
     GaussianNB()
 ]
 
-# Split the data into train and test sets
-X = mushroom_data.drop('class', axis=1)
-y = mushroom_data['class']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Print Logistic Regression Visual
+#dataPrinting.print_logistic_regression_visual(X, X_train, y_train)
 
 # Lists to store model names and their corresponding accuracies
 model_names = []
 accuracies = []
+confusions = []
+reports = []
 
 # Iterate through the list of models and train/evaluate each one
 for model in models:
-    # Train the model on the training data
-    model.fit(X_train, y_train)
+    # Scale the data before training the model
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
-    # Make predictions on the test data
-    y_pred = model.predict(X_test)
+    # Train the model on the scaled training data
+    model.fit(X_train_scaled, y_train)
+
+    # Make predictions on the scaled test data
+    y_pred = model.predict(X_test_scaled)
 
     # Evaluate the model's performance
     accuracy = accuracy_score(y_test, y_pred)
@@ -144,6 +151,8 @@ for model in models:
     # Store model name and accuracy
     model_names.append(model.__class__.__name__)
     accuracies.append(accuracy)
+    confusions.append(confusion)
+    reports.append(report)
 
     # Print the results for each model
     print(f"Model: {model.__class__.__name__}")
@@ -152,13 +161,15 @@ for model in models:
     print("Classification Report:\n", report)
     print("\n")
 
-dataPrinting.print_compare_model_accuracies(model_names, accuracies)
+#dataPrinting.print_compare_model_accuracies(model_names, accuracies)
 
 
 # Lists to store model names and their corresponding fold accuracies
-model_names = []
+cv_model_names = []
 fold_accuracies = []
 mean_accuracies = []
+fold_confusions = []
+fold_reports = []
 
 # Specify the number of folds for cross-validation
 num_folds = 5  # You can adjust this number as needed
@@ -168,45 +179,57 @@ cv = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=42)
 
 # Iterate through the list of models and perform cross-validation
 for model in models:
+    # Initialize lists to store fold-specific results
+    fold_accuracies_model = []
+    fold_confusions_model = []
+    fold_reports_model = []
+    
     # Perform cross-validation and calculate accuracy scores for each fold
-    fold_scores = cross_val_score(model, X, y, cv=cv, scoring='accuracy')
+    for fold_num, (train_idx, test_idx) in enumerate(cv.split(X, y), start=1):
+        X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+        y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
+        
+        # Train the model on the training data
+        model.fit(X_train, y_train)
+
+        # Make predictions on the test data
+        y_pred = model.predict(X_test)
+
+        # Evaluate the model's performance
+        accuracy = accuracy_score(y_test, y_pred)
+        confusion = confusion_matrix(y_test, y_pred)
+        report = classification_report(y_test, y_pred)
+        
+        # Store fold-specific results
+        fold_accuracies_model.append(accuracy)
+        fold_confusions_model.append(confusion)
+        fold_reports_model.append(report)
     
     # Store model name and fold accuracies
-    model_names.append(model.__class__.__name__)
-    fold_accuracies.append(fold_scores)
+    cv_model_names.append(model.__class__.__name__)
+    fold_accuracies.append(fold_accuracies_model)
+    fold_confusions.append(fold_confusions_model)
+    fold_reports.append(fold_reports_model)
     
     # Calculate and store mean accuracy across all folds
-    mean_accuracy = fold_scores.mean()
+    mean_accuracy = np.mean(fold_accuracies_model)
     mean_accuracies.append(mean_accuracy)
 
-# Print the cross-validation results for each model and each fold
-for model_name, fold_accuracy in zip(model_names, fold_accuracies):
+# Print the results for each fold and model at the end
+for model_name, fold_accuracy, fold_confusion, fold_report in zip(cv_model_names, fold_accuracies, fold_confusions, fold_reports):
     print(f"Model: {model_name}")
-    for fold_num, accuracy in enumerate(fold_accuracy, start=1):
+    for fold_num, (accuracy, confusion, report) in enumerate(zip(fold_accuracy, fold_confusion, fold_report), start=1):
         print(f"Fold {fold_num} Accuracy: {accuracy:.4f}")
-    print(f"Mean Accuracy: {mean_accuracies[model_names.index(model_name)]:.4f}\n")
+        print("Confusion Matrix:\n", confusion)
+        print("Classification Report:\n", report)
+        print("\n")
+    print(f"Mean Accuracy: {mean_accuracies[cv_model_names.index(model_name)]:.4f}\n")
 
 
-
-# DECISIONTREECLASSIFIER VISUAL
-# Create a DecisionTreeClassifier
-dt_classifier = DecisionTreeClassifier(random_state=42)
-
-# Fit the model to your data
-dt_classifier.fit(X_train, y_train)
-
-# Plot the decision tree
-plt.figure(figsize=(10, 6))
-plot_tree(dt_classifier, filled=True, feature_names=list(X.columns), class_names=["EDIBLE", "POISONOUS"])
-plt.title("Decision Tree")
-plt.show()
-
-print(mean_accuracies)
-
-
+'''
 #LOGISTIC REGRESSION MODEL VISUAL
 # Create a Logistic Regression model
-logistic_regression = LogisticRegression(random_state=42)
+logistic_regression = LogisticRegression(random_state=42, max_iter=1000)
 
 # Train the model
 logistic_regression.fit(X_train, y_train)
@@ -253,4 +276,49 @@ for i, tree in enumerate(rf_classifier.estimators_[:num_decision_trees_to_visual
     plot_tree(tree, filled=True, feature_names=list(X.columns), class_names=["EDIBLE", "POISONOUS"])
     plt.title(f"Decision Tree {i + 1}")
 plt.tight_layout()
+plt.show()
+'''
+
+dataPrinting.print_decision_tree_classifer(X, cv_model_names, models)
+print(mean_accuracies)
+
+
+#CROSS VALIDATION DATA ANALYSIS
+#dataPrinting.print_crossfold_data(cv_model_names, fold_accuracies, mean_accuracies, num_folds)
+#dataPrinting.print_crossfold_compare_all_same_graph(cv_model_names, fold_accuracies, mean_accuracies, num_folds)
+#dataPrinting.testing(cv_model_names, fold_accuracies, mean_accuracies, num_folds)
+
+#dataPrinting.print_crossfold_compare_separate(cv_model_names, fold_accuracies, mean_accuracies, num_folds)
+
+
+#LOGISTIC REGRESSION MODEL VISUAL
+
+# After the loop, you can access the logistic_regression model
+logistic_regression = models[1]  # Assuming logistic regression is the second model in the list
+
+# Now you can access the coefficients of the logistic_regression model
+coefficients = logistic_regression.coef_[0]
+
+# Match coefficients with feature names
+feature_names = list(X.columns)
+
+# Create a DataFrame to store feature names and their corresponding coefficients
+coef_df = pd.DataFrame({'Feature': feature_names, 'Coefficient': coefficients})
+
+# Sort the DataFrame by coefficient values (absolute values for magnitude)
+coef_df['Abs_Coefficient'] = np.abs(coef_df['Coefficient'])
+
+# Print the DataFrame to see the coefficients for each feature
+print(coef_df)
+sorted_coef_df = coef_df.sort_values(by='Abs_Coefficient', ascending=False)
+
+# Plot the top N most important features
+N = 10  # You can adjust this value
+top_features = sorted_coef_df.head(N)
+
+plt.figure(figsize=(10, 6))
+sns.barplot(x='Coefficient', y='Feature', data=top_features)
+plt.title('Top {} Features - Logistic Regression Coefficients'.format(N))
+plt.xlabel('Coefficient Value')
+plt.ylabel('Feature')
 plt.show()
--- printing.py
+++ printing.py
@@ -5,6 +5,10 @@
 # print the decision tree graph to see what the nodes it split on are
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+from sklearn.tree import plot_tree
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
 
 def print_class_compare(mushroom_data, title):
     # Count the frequency of each class
@@ -33,13 +37,130 @@ def print_compare_model_accuracies(model_names, accuracies):
     plt.title('Model Comparison - Accuracy')
     plt.xticks(rotation=45)  # Rotate x-axis labels for readability
     plt.tight_layout()
-
-    # Show the bar chart
     plt.show()
 
+# Print Heatmap
 def print_heatmap(mushroom_data):
     # check to get a rough idea of correlations
     correlations = mushroom_data.corr()
     plt.subplots(figsize=(20, 15))
     data_corr = sns.heatmap(correlations, annot=True, linewidths=0.5, cmap="RdBu_r")
+    plt.show()
+
+def print_crossfold_data(cv_model_names, fold_accuracies, mean_accuracies, num_folds):
+    # Iterate through models to create individual plots
+    for i, model_name in enumerate(cv_model_names):
+        plt.figure(figsize=(10, 6))
+        plt.title(f'Mean and Individual Fold Accuracies for {model_name}')
+        plt.xlabel('Fold')
+        plt.ylabel('Accuracy')
+
+        # Plot individual fold accuracies
+        fold_accuracy = fold_accuracies[i]
+        x_values = np.arange(1, num_folds + 1)
+        plt.scatter(x_values, fold_accuracy, label=f'Fold Accuracies ({model_name})', marker='o')
+
+        # Plot the mean accuracy
+        mean_accuracy = mean_accuracies[i]
+        plt.plot(x_values, [mean_accuracy] * num_folds, label=f'Mean Accuracy ({model_name})', linestyle='--')
+
+        plt.xticks(x_values)
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+
+
+def print_crossfold_compare_all_same_graph(cv_model_names, fold_accuracies, mean_accuracies, num_folds):
+    plt.figure(figsize=(10, 6))
+    
+    for i, model_name in enumerate(cv_model_names):
+        # Plot individual fold accuracies
+        fold_accuracy = fold_accuracies[i]
+        x_values = np.arange(1, num_folds + 1)
+        plt.scatter(x_values, fold_accuracy, label=f'Fold Accuracies ({model_name})', marker='o')
+
+        # Plot the mean accuracy
+        mean_accuracy = mean_accuracies[i]
+        plt.plot(x_values, [mean_accuracy] * num_folds, label=f'Mean Accuracy ({model_name})', linestyle='--')
+
+    plt.title('Mean and Individual Fold Accuracies for Models')
+    plt.xlabel('Fold')
+    plt.ylabel('Accuracy')
+    plt.xticks(x_values)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+def print_crossfold_compare_separate(cv_model_names, fold_accuracies, mean_accuracies, num_folds):
+    figsize=(10, 6)
+    num_models = len(cv_model_names)
+    
+    # Define default colors
+    colors = ['b', 'g', 'r', 'c', 'm']  # You can customize these colors
+    
+    fig, axes = plt.subplots(num_models, 1, figsize=(figsize[0], figsize[1]), sharex=True, sharey=True)
+    
+    for i, model_name in enumerate(cv_model_names):
+        # Plot individual fold accuracies
+        fold_accuracy = fold_accuracies[i]
+        x_values = np.arange(1, num_folds + 1)
+        axes[i].scatter(x_values, fold_accuracy, label=f'Fold Accuracies ({model_name})', marker='o', color=colors[i])
+
+        # Plot the mean accuracy
+        mean_accuracy = mean_accuracies[i]
+        axes[i].plot(x_values, [mean_accuracy] * num_folds, label=f'Mean Accuracy ({model_name})', linestyle='--', color=colors[i])
+
+        axes[i].set_title(f'Mean and Individual Fold Accuracies for {model_name}')
+        axes[i].set_ylabel('Accuracy')
+        axes[i].legend()
+
+    plt.xlabel('Fold')
+    plt.tight_layout()
+    plt.show()
+
+
+def print_decision_tree_classifer(X, cv_model_names, models):
+    # Define the name of the model you want to visualize
+    model_name_to_visualize = "DecisionTreeClassifier"
+
+    # Find the index of the model with the specified name in the list of model names
+    model_index = cv_model_names.index(model_name_to_visualize)
+
+    # Plot the decision tree for the specified model
+    plt.figure(figsize=(10, 6))
+    plot_tree(models[model_index], filled=True, feature_names=list(X.columns), class_names=["EDIBLE", "POISONOUS"])
+    plt.title(f"Decision Tree for {model_name_to_visualize}")
+    plt.show()
+
+def print_logistic_regression_visual(X, X_train, y_train):
+    #LOGISTIC REGRESSION MODEL VISUAL
+    # Create a Logistic Regression model
+    logistic_regression = LogisticRegression(random_state=42, max_iter=1000)
+
+    # Train the model
+    logistic_regression.fit(X_train, y_train)
+
+    # Get the coefficients (weights) of the model
+    coefficients = logistic_regression.coef_[0]
+
+    # Match coefficients with feature names
+    feature_names = list(X.columns)
+
+    # Create a DataFrame to store feature names and their corresponding coefficients
+    coef_df = pd.DataFrame({'Feature': feature_names, 'Coefficient': coefficients})
+
+    # Sort the DataFrame by coefficient values (absolute values for magnitude)
+    coef_df['Abs_Coefficient'] = np.abs(coef_df['Coefficient'])
+    sorted_coef_df = coef_df.sort_values(by='Abs_Coefficient', ascending=False)
+
+    # Plot the top N most important features
+    N = 10  # You can adjust this value
+    top_features = sorted_coef_df.head(N)
+
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='Coefficient', y='Feature', data=top_features)
+    plt.title('Top {} Features - Logistic Regression Coefficients'.format(N))
+    plt.xlabel('Coefficient Value')
+    plt.ylabel('Feature')
     plt.show()
